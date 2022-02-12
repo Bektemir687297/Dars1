5b2ba837329726c541236791eeccb3bda80273ad
Bektemri <bek.abduaimov@gmail.com>
2022-02-12 15:59:23 +0500

    Bek

---
+++ script.js
@@ -0,0 +1,19 @@
+"use strict";
+
+//console.log("Hello world");
+
+//alert("Hello world");
+
+//const userAge = confirm("Siz 18 yoshdamisizi?");
+//console.log(userAge);
+
+//const userName = prompt("Ismingiz nima?", "");
+
+//console.log(userName);
+
+const answers = [];
+
+answers[0] = prompt("Ismingiz nima?");
+answers[1] = prompt("Familngiz nima?");
+answers[2] = prompt("Yoshingiz nechida?");
+console.log(answers);
